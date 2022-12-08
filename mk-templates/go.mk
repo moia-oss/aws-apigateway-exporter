3dabd9471cc665209d02f9365ed751aa5bbd8acf
Moritz Siuts <moritzs@moia.io>
2022-12-08 09:43:13 +0100

    bump make files for Go 1.19

--- mk-templates/go.mk
+++ mk-templates/go.mk
@@ -1,24 +1,22 @@
 # This overrides the default go command withenv Variables which are usually
 # used in moia-dev repositories
-SYSTEM                := $(shell uname -s | tr A-Z a-z)_$(shell uname -m | sed "s/x86_64/amd64/")
+SYSTEM                := $(shell uname -s | tr A-Z a-z)_$(shell uname -m | sed "s/x86_64/amd64/" | sed "s/aarch64/arm64/")
 GO_PREFIX             := CGO_ENABLED=0 GOFLAGS=-mod=vendor GOPRIVATE=github.com/moia-dev
 GO                    := $(GO_PREFIX) go
 # This collects every path, which contains go files in the current project
-LINT_TARGETS          ?= $(shell $(GO) list -f '{{.Dir}}' ./... | sed -e"s|${CURDIR}/\(.*\)\$$|\1/...|g" | grep -v node_modules/ )
-# The current version of golangci-lint. 
+LINT_TARGETS          := $(shell find -name '*.go' | sed -e "s|\(.*\)/.*\.go\$$|\1/...|g" | grep -v vendor | grep -v node_modules | uniq)
+# The current version of golangci-lint.
 # See: https://github.com/golangci/golangci-lint/releases
-GOLANGCI_LINT_VERSION := 1.30.0
+GOLANGCI_LINT_VERSION ?= 1.49.0
 
 # Executes the linter on all our go files inside of the project
-.PHONY: lint
+.PHONY: lint create-golint-config
 lint: bin/golangci-lint-$(GOLANGCI_LINT_VERSION)
 	$(GO_PREFIX) ./bin/golangci-lint-$(GOLANGCI_LINT_VERSION) --timeout 120s run $(LINT_TARGETS)
 
 .PHONY: create-golint-config
-create-golint-config: .golangci.yml
-
-.golangci.yml:
-	cp moia-mk-templates/assets/golangci.yml $@
+create-golint-config:
+	cp -n moia-mk-templates/assets/golangci.yml .golangci.yml || true
 
 # Downloads the current golangci-lint executable into the bin directory and
 # makes it executable
@@ -33,3 +31,7 @@ PHONY: dependabot-batch
 dependabot-batch:
 	$(eval NOW := $(shell date +%s))
 	git checkout -b update-deps-$(NOW) && for i in $(shell git branch -a | grep "/origin/dependabot/go_modules") ; do git cherry-pick $$i ; done && go mod vendor && go mod tidy && git add vendor go.mod go.sum && git commit -m "vendor dependencies" && git push --set-upstream origin update-deps-$(NOW)
+
+.PHONY: test
+test:
+	$(GO) test $(LINT_TARGETS)
